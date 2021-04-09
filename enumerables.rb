f2c7189f9d4bc98cdcec7c28e9e96cf536f0c923
Chike1990 <chibuikeuzoechina@gmail.com>
2021-04-09 16:46:42 +0100

    completes my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -9,8 +9,16 @@ module Enumerable
     self
     end
 
-
-    [1,2,3,4,5].my_each {|number| p number*number}
-    [1,2,3,4,5].my_each { puts 'Chike & Erasmus' }
+    #[1,2,3,4,5].my_each {|number| p number*number}
+    #[1,2,3,4,5].my_each { puts 'Chike & Erasmus' }
     
+    def my_each_with_index
+        return enum_for(:my_each_with_index) unless block_given?
+      i = 0
+      while i < to_a.length
+        yield(to_a[i])
+        i += 1
+      end
+      self
+      end
 end
