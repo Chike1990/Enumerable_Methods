705ff14e260794036ab52234d726d82a09f459fa
Chike1990 <chibuikeuzoechina@gmail.com>
2021-04-09 16:33:20 +0100

    my_method

---
+++ enumerables.rb
@@ -0,0 +1,15 @@
+module Enumerable
+    def my_each
+      return enum_for(:my_each) unless block_given?
+    i = 0
+    while i < to_a.length
+      yield(to_a[i])
+      i += 1
+    end
+    self
+    end
+
+
+    [1,2,3,4,5].my_each {|number| p number*number}
+    [1,2,3,4,5].my_each { puts 'Chike & Erasmus' }
+end
