f4986b7c393b7fe36f6f7d2e9a232c0d4364c716
Chike1990 <chibuikeuzoechina@gmail.com>
2021-04-17 20:41:12 +0100

    Adds My_any method

--- enumerables.rb
+++ enumerables.rb
@@ -1,37 +1,43 @@
 module Enumerable
-    def my_each
-      return enum_for(:my_each) unless block_given?
+  def my_each
+    return enum_for(:my_each) unless block_given?
+
     i = 0
     while i < to_a.length
       yield(to_a[i])
       i += 1
     end
     self
+  end
+
+  # [1,2,3,4,5].my_each {|number| p number*number}
+  # [1,2,3,4,5].my_each { puts 'Chike & Erasmus' }
+
+  def my_each_with_index
+    return enum_for(:my_each_with_index) unless block_given?
+
+    i = 0
+    while i < to_a.length
+      yield(to_a[i])
+      i += 1
     end
+    self
+  end
+  # [1,2,3,4,5].my_each_with_index {|index ,number| p index *2}
+  # %w[Chike emmy 123 erasmus cake].my_each_with_index {|x| puts "hello #{x}"}
 
-    #[1,2,3,4,5].my_each {|number| p number*number}
-    #[1,2,3,4,5].my_each { puts 'Chike & Erasmus' }
-    
-    def my_each_with_index
-        return enum_for(:my_each_with_index) unless block_given?
-      i = 0
-      while i < to_a.length
-        yield(to_a[i])
-        i += 1
-      end
-      self
-      end
-   # [1,2,3,4,5].my_each_with_index {|index ,number| p index *2}
-  #%w[Chike emmy 123 erasmus cake].my_each_with_index {|x| puts "hello #{x}"}
-
-  
   def my_select
     return enum_for(:my_select) unless block_given?
+
     arr = []
     to_a.my_each { |n| arr.push(n) if yield(n) }
     arr
-   end
+    end
+  end
+ # [8,2,5,1,6,3].my_select{|n| n if  p n == }
 end
 
- #[8,2,5,1,6,3].my_select{|n| n if  p n == }
+
+def my_all
+  #some code/block
 end
